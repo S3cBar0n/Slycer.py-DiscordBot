ccd536500f639dacf3390227c5a5e0d1086c7610
S3cBar0n <secberg@gmail.com>
2020-04-16 19:21:48 -0600

    Initial commit

---
+++ baron_bot/bot.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from discord.ext import commands
+
+# Prefix for my commands
+client = commands.Bot(command_prefix = "!")
+
+
+# Loads our cogs library
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+# Unloads our cogs library
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+
+
+
+# Searches for .py files within the cogs directory on the OS and removes .py when loaded
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"cogs.{filename[:-3]}")
+
+client.run("Njk5Njg2NTUyNTIzMjQzNTUx.XpYAAg.BSlb7pzR0xJlAyZJ_9KZ8NedK_8")
---
+++ baron_bot/cogs/commands.py
@@ -0,0 +1,102 @@
+import discord
+import random
+import datetime
+import calendar
+from discord.ext import commands
+
+# BEGIN VARIABLES
+dateToday = datetime.date.today()
+
+# Calculates how many days are in the current month
+dateDaysinMonth = calendar.monthrange(dateToday.year, dateToday.month)[1]
+
+# Variables to Calculate days until Summer Vacation
+dateSchool = datetime.date(2020, 5, 15) - datetime.date.today()
+dateSchoolStr = str(dateSchool)
+dateSummer = dateSchoolStr.strip("0: ,")
+dateSummerSentance = " until summer!"
+dateSchoolEnd = dateSummer + dateSummerSentance
+
+# Variables to Calculate how many days until the end of the month
+dateDaysUntilMonthEnd = dateDaysinMonth - dateToday.day + 1
+dateMonthEndStr = str(dateDaysUntilMonthEnd)
+dateSentance = " Days until the month ends!"
+dateMonthEnd = dateMonthEndStr + dateSentance
+
+
+
+
+class Commands(commands.Cog):
+# This refrences the client we created within our bot.py and passes it into the cog
+    def __init__(self, client):
+        self.client = client  # this allows us to access the client within our cog
+
+# Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Logged in as " + self.client.user.name)
+        print(self.client.user.id)
+        print("-------")
+
+
+# Commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("pong!")
+
+    @commands.command()
+    async def aboutbot(self, ctx):
+        await ctx.send("I am a clever Discord Bot that was put together as part of a school project by my creator @Baron!")
+
+    @commands.command()
+    async def dumb(self, ctx):
+        await ctx.send("I am a dumb robot!")
+
+    @commands.command()
+    async def summer(self, ctx):
+        await ctx.send(dateSchoolEnd)
+
+    @commands.command()
+    async def nextmonth(self, ctx):
+        await ctx.send(dateMonthEnd)
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+    @commands.command(aliases=['8ball'])
+    async def _8ball(self, ctx, *, question):
+        responses = ["It is certain.",
+              "It is decidedly so.",
+               "Without a doubt.",
+               "Yes - definitely.",
+               "You may rely on it.",
+               "As I see it, yes.",
+               "Most likely.",
+               "Outlook good.",
+               "Yes.",
+               "Signs point to yes.",
+               "Reply hazy, try again.",
+               "Ask again later.",
+               "Better not tell you now.",
+               "Cannot predict now.",
+               "Concentrate and ask again.",
+               "Don't count on it.",
+               "My reply is no.",
+               "My sources say no.",
+               "Outlook not so good.",
+               "Very doubtful.",]
+        await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+
+
+
+
+
+
+
+
+
+
+# This function allows us to connect this cog to our bot
+def setup(client):
+    client.add_cog(Commands(client))
