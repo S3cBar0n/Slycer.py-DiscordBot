f12971f3cb09503ed6e9e77c2a27c9fcd50fa37a
S3cBar0n <secberg@gmail.com>
2020-04-16 20:34:59 -0600

    Removed token from script

--- baron_bot/bot.py
+++ baron_bot/bot.py
@@ -1,6 +1,7 @@
-import discord
 import os
+from baron_bot.TOKEN import TOKEN
 from discord.ext import commands
+
 
 # Prefix for my commands
 client = commands.Bot(command_prefix = "!")
@@ -22,4 +23,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-client.run("Njk5Njg2NTUyNTIzMjQzNTUx.XpkJxw.NoDpTR_GJ-QHj7_7FQxjQJlyfpI")
+client.run(TOKEN)
