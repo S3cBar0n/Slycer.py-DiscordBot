6ad7ead1ee32b628603054e42de5bf0e3ea5f5f4
S3cBar0n <secberg@gmail.com>
2020-04-18 16:40:21 -0600

    Revert "Changed f"\song to f"/song"
    This reverts commit ee0c5c99

--- baron_bot/cogs/music.py
+++ baron_bot/cogs/music.py
@@ -48,9 +48,9 @@ class Music(commands.Cog):
     async def play(self, ctx, url: str):
 
         def check_queue():
-            queue_infile = os.path.isdir("/Queue")
-            if queue_infile is True:
-                DIR = os.path.abspath(os.path.realpath("/Queue"))
+            Queue_infile = os.path.isdir("./Queue")
+            if Queue_infile is True:
+                DIR = os.path.abspath(os.path.realpath("./Queue"))
                 length = len(os.listdir(DIR))
                 still_q = length - 1
                 try:
@@ -60,7 +60,7 @@ class Music(commands.Cog):
                     queues.clear()
                     return
                 main_location = os.path.dirname(os.path.realpath(__file__))
-                song_path = os.path.abspath(os.path.realpath("/Queue") + "//" + first_file)
+                song_path = os.path.abspath(os.path.realpath("./Queue") + "\\" + first_file)
                 if length != 0:
                     print("Preparing next song.\n")
                     print(f"Songs still in queue: {still_q}")
@@ -94,10 +94,10 @@ class Music(commands.Cog):
             await ctx.send("ERROR: Music is currently being played.")
             return
 
-        queue_infile = os.path.isdir("/Queue")
+        Queue_infile = os.path.isdir("./Queue")
         try:
-            Queue_folder = "/Queue"
-            if queue_infile is True:
+            Queue_folder = "./Queue"
+            if Queue_infile is True:
                 print("Removed old Queue folder")
                 shutil.rmtree(Queue_folder)
         except:
@@ -190,13 +190,29 @@ class Music(commands.Cog):
             print("No Music playing - Music failed to stop.")
             await ctx.send("No music currently playing, failed to stop.")
 
+    # Skip Command
+    @commands.command(pass_context=True, aliases=["s"])
+    async def stop(self, ctx):
+
+        voice = get(self.client.voice_clients, guild=ctx.guild)
+
+        queues.clear()
+
+        if voice and voice.is_playing():
+            print("Music stopped.")
+            voice.stop()
+            await ctx.send("Music has been stopped.")
+        else:
+            print("No Music playing - Music failed to stop.")
+            await ctx.send("No music currently playing, failed to stop.")
+
     # Queue
     @commands.command(pass_context=True, aliases=["q"])
     async def queue(self, ctx, url: str):
-        queue_infile = os.path.isdir("/Queue")
-        if queue_infile is False:
+        Queue_infile = os.path.isdir("./Queue")
+        if Queue_infile is False:
             os.mkdir("Queue")
-        DIR = os.path.abspath(os.path.realpath("/Queue"))
+        DIR = os.path.abspath(os.path.realpath("./Queue"))
         q_num = len(os.listdir(DIR))
         q_num += 1
         add_queue = True
@@ -207,7 +223,7 @@ class Music(commands.Cog):
                 add_queue = False
                 queues[q_num] = q_num
 
-        queue_path = os.path.abspath(os.path.realpath("/Queue") + f"/song{q_num}.%(ext)s")
+        queue_path = os.path.abspath(os.path.realpath("./Queue") + f"\song{q_num}.%(ext)s")
 
         # Begins downloading the youtube file and converts to MP3
         ydl_opts = {
