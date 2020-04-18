48ef278abb8b6aa3b18cbf1e7a9776d6f3deba3a
S3cBar0n <secberg@gmail.com>
2020-04-18 00:57:36 -0600

    Added Music Cog

--- baron_bot/cogs/music.py
+++ baron_bot/cogs/music.py
@@ -1,5 +1,7 @@
-import youtube_dl
+import os
+# import youtube_dl
 from discord.ext import commands
+from discord.utils import get
 
 # ------------------- Variables -------------------
 players = {}
@@ -9,26 +11,19 @@ class Music(commands.Cog):
     def __init__(self, client):
         self.client = client  # this allows us to access the client within our cog
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, aliases=["j"])
     async def join(self, ctx):
-        channel = ctx.author.voice.channel
-        await channel.connect()
+        global voice
+        channel = ctx.message.author.voice.channel
+        voice = get(self.client.voice_clients, guild=ctx.guild)
 
-    @commands.command(pass_context=True)
-    async def leave(self, ctx):
-        await ctx.voice_client.disconnect()
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+            print(f"DJ Baron Bot has connected to {channel}\n")
 
-
-    @commands.command(pass_context=True)
-    async def play(self, ctx, url):
-        server = self.ctx.message.server
-        voice_client = self.client.voice_client_in(server)
-        player = await voice_client.create_ytdl_player(url)
-        players[server.id] = player
-        player.start()
-
-
-
+        await ctx.send(f"Joined {channel}")
 
 
 
